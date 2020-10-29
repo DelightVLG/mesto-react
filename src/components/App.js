abd0b0ba49019ff13072b4e496f19bdc3f015a49
Sergei Kompanietc <delightvlg@gmail.com>
2020-10-30 02:42:01 +0300

    add modals states -> open

--- src/components/App.js
+++ src/components/App.js
@@ -27,11 +27,61 @@ function App() {
       <div className="page">
         <div className="page__container">
           <Header />
-          <Main onEditAvatar={handleEditAvatarClick}
-                onEditProfile={handleEditProfileClick}
-                onAddPlace={handleAddPlaceClick}/>
+
+          <Main
+            onEditAvatar={handleEditAvatarClick}
+            onEditProfile={handleEditProfileClick}
+            onAddPlace={handleAddPlaceClick}/>
+
           <Footer copyright="© 2020 Mesto Russia. Сергей Компаниец" />
-          <ModalWithForm />
+
+          <ModalWithForm
+            name={'edit-avatar'}
+            title={'Обновить аватар?'}
+            children={
+            <>
+              <input type="url" className="modal__input-txt modal__input-txt_type_edit-avatar" value="" name="link"
+                     placeholder="Ссылка на новое фото"/>
+              <span className="modal__input-error" id="link-error"/>
+              <input type="submit" className="modal__sbmt-btn" value="Сохранить" name="save" disabled />
+            </>
+            }
+           isOpen={isEditAvatarModalOpen}/>
+
+          <ModalWithForm
+          name={'edit-profile'}
+          title={'Редактировать профиль'}
+          children={
+            <>
+            <input type="text" className="modal__input-txt modal__input-txt_type_name" value="" name="name"
+                   placeholder="Имя" required minLength="2" maxLength="40" autoComplete="off" />
+              <span className="modal__input-error" id="name-error"/>
+            <input type="text" className="modal__input-txt modal__input-txt_type_job" value="" name="about"
+                   placeholder="Ваша профессия?" required minLength="2" maxLength="200" autoComplete="off" />
+              <span className="modal__input-error" id="about-error"/>
+            <input type="submit" className="modal__sbmt-btn" value="Сохранить" name="save" disabled />
+            </>
+          }
+          isOpen={isEditProfileModalOpen}
+          />
+
+          <ModalWithForm
+            name={'add-place'}
+            title={'Новое место'}
+            children={
+              <>
+              <input type="text" className="modal__input-txt modal__input-txt_type_place-name"
+                     value="" name="placeName" placeholder="Название" required minLength="1"
+                     maxLength="30" />
+                <span className="modal__input-error" id="placeName-error"/>
+              <input type="url" className="modal__input-txt modal__input-txt_type_img-url"
+                     value="" name="placeUrl" placeholder="Ссылка на картинку" required />
+              <span className="modal__input-error" id="placeUrl-error" />
+              <input type="submit" className="modal__sbmt-btn" value="Создать" name="save" />
+              </>
+            }
+            isOpen={isAddPlaceModalOpen}
+            />
         </div>
       </div>
     </>
@@ -39,3 +89,6 @@ function App() {
 }
 
 export default App;
+
+// edit-profile edit-avatar add-place del-submit
+// Редактировать профиль Обновить аватар Новое место Вы уверены?
--- src/components/ModalWithForm.js
+++ src/components/ModalWithForm.js
@@ -3,11 +3,9 @@ import React from 'react';
 const ModalWithForm = ({ name, title, children, isOpen }) => {
   return (
     <div className={`modal modal_type_${name} ${isOpen ? 'modal_is-open' : 'lol'}`}>
-      // edit-profile edit-avatar add-place del-submit
       <div className="modal__container">
         <span className="modal__close-btn" />
         <h2 className="modal__title">{title}</h2>
-        // Редактировать профиль Обновить аватар Новое место Вы уверены?
         <form className="modal__form" name={name} action="#">
           {children}
         </form>
