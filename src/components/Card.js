6a124a3406ec548b816a74061cd37bc1fa55bc42
Sergei Kompanietc <delightvlg@gmail.com>
2020-10-30 17:41:45 +0300

    add cards and spinner

---
+++ src/components/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = (card) => {
+  return (
+    <>
+      <li className="elements__element" key={card.id}>
+        <img src={card.link} alt="" className="elements__img" />
+        <div className="elements__footer">
+          <h2 className="elements__title">{card.name}</h2>
+          <div className="elements__like-container">
+            <button className="elements__like-btn"/>
+            <p className="elements__like-counter">{card.likes}</p>
+          </div>
+          <button className="elements__del-btn"/>
+        </div>
+      </li>
+    </>
+  )
+}
+
+export default Card;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../utils/api';
+import Card from './Card';
+import Spinner from './Spinner';
 
 const Main = ({ onEditAvatar, onEditProfile, onAddPlace }) => {
+  console.log('MAIN RENDERED');
+
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState();
+  const [cards, setCards] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then((data => {
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
+      }))
+  }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    api.getInitialCardList()
+      .then((data) => {
+        const items = data.map(item => ({
+          id: item._id,
+          name: item.name,
+          link: item.link,
+          likes: item.likes.length
+          }));
+        setIsLoading(false)
+        setCards(items);
+      });
+  },[])
 
   return (
     <main className="page__main">
       <section className="profile page__profile">
         <div className="profile__info">
           <div onClick={onEditAvatar} className="profile__photo-container">
-            <img className="profile__photo" src="#" alt="Фотография профиля" />
+            <img className="profile__photo" src={userAvatar} alt="Фотография профиля" />
           </div>
           <div className="profile__heading">
-            <h1 className="profile__title">Sergei Kompanietc</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button onClick={onEditProfile} className="profile__edit-btn" type="button" aria-label="Edit" />
           </div>
-          <p className="profile__subtitle">Junior front-end engineer</p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button onClick={onAddPlace} className="profile__add-btn" type="button" aria-label="Add" />
       </section>
 
       <section className="page__elements">
-        <ul className="elements" />
+        <ul className="elements">
+          {isLoading ? <Spinner /> : cards.map(card => <Card key={card.id} {...card}/> )}
+        </ul>
       </section>
 
       {/*<div className="modal modal_type_preview">*/}
